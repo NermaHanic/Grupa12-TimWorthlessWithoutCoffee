ea3b266849eb4660c824e8dcb04e01481e17d09b
Amila Borancic <aborancic1@etf.unsa.ba>
2018-03-31 10:09:26 +0200

    Blank UWP project

--- ProjekatDearWalletDressMeUp/ProjekatDearWalletDressMeUp/App.xaml.cs
+++ ProjekatDearWalletDressMeUp/ProjekatDearWalletDressMeUp/App.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-namespace blank_UWP
+namespace ProjekatDearWalletDressMeUp
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
