515b3cd74b9714741f82f0081c4bc7dfe2a470c4
NermaHanic <nhanic2@etf.unsa.ba>
2018-05-16 16:21:34 +0200

    Dodate pomocne klase za projekat

---
+++ DearWalletDressMeUp/DearWalletDressMeUp/Helper/INavigacija.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DearWalletDressMeUp.Helper
+{
+    interface INavigacija
+    {
+        void Navigiraj(Type sourcePage);
+        void Navigiraj(Type sourcePage, object parameter);
+        void Nazad();
+    }
+}
+
--- DearWalletDressMeUp/DearWalletDressMeUp/ViewModel/KorisnikViewModel.cs
+++ DearWalletDressMeUp/DearWalletDressMeUp/ViewModel/KorisnikViewModel.cs
@@ -1,20 +1,89 @@
-﻿using System;
+﻿using DearWalletDressMeUp.Model;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
 using System.Windows.Input;
-using DearWalletDressMeUp.Model;
+using Microsoft.WindowsAzure.MobileServices;
 
 namespace DearWalletDressMeUp.ViewModel
 {
     public class KorisnikViewModel
     {
-        /* ICommand DobaviIzBaze { get; set; }
-         ICommand UnesiUBazu { get; set; }
-         ICommand UnesiViseUBazu { get; set; }*/
+         //public ICommand DobaviIzBaze { get; set; }
+         public ICommand UnesiUBazu { get; set; }
+         //public ICommand UnesiViseUBazu { get; set; }
         List<Korisnik> korisnici;
 
+        public KorisnikViewModel()
+        {
+            //DobaviIzBaze = new RelayCommand();
+            UnesiUBazu = new RelayCommand(dodajUBazu);
+            //UnesiViseUBazu = new RelayCommand();
+            korisnici = new List<Korisnik>();
+        }
 
+        IMobileServiceTable<Korisnik> tabelica = App.MobileService.GetTable<Korisnik>();
+
+        public void dodajUBazu(object parametar)
+        {
+            error.Text = "";
+            if (ime.Text.Length == 0 || prezime.Text.Length == 0 || sifra.Password.Length == 0 || potvrdaSifre.Password.Length == 0 || adresa.Text.Length == 0 || telefon.Text.Length == 0)
+            {
+                error.Text = "Niste unijeli sva obavezna polja.";
+            }
+
+            else if (potvrdaSifre.Password != sifra.Password)
+            {
+                error.Text = "Unesene sifre se ne slazu.";
+            }
+
+            else
+            {
+                for (int i = 0; i < telefon.Text.Length; i++)
+                {
+                    if (Char.IsLetter(telefon.Text[i])) error.Text = "Neispravan format broja telefona";
+                }
+                for (int i = 0; i < kartica.Text.Length; i++)
+                {
+                    if (Char.IsLetter(kartica.Text[i])) error.Text = "Neispravan format broja kreditne kartice";
+                }
+            }
+
+            if (error.Text == "")
+            {
+                try
+                {
+                    Korisnik obj = new Korisnik(ime.Text, prezime.Text, EmailReg.Text, adresa.Text, telefon.Text, kartica.Text,
+                        await Pomocna.DodjelaUsername(ime.Text, prezime.Text), sifra.Password);
+                    await tabelica.InsertAsync(obj);
+
+                    /*IMobileServiceTable<Profil> profil = App.MobileService.GetTable<Profil>();
+                    Profil objProf = new Profil();
+                    objProf.Id = await ID<Profil>.DodjelaID(profil);
+                    await profil.InsertAsync(objProf);*/
+
+                    MessageDialog m = new MessageDialog("Uspjesno ste registrovani na Dear Wallet Dress Me Up! Vas username glasi: " + obj.Id + " :)");
+                    await m.ShowAsync();
+                    this.Frame.Navigate(typeof(Home));
+                }
+                catch (Exception ex)
+                {
+                    MessageDialog feedbackError = new MessageDialog("Error : Doslo je do greske! \n" + ex.ToString());
+                    await feedbackError.ShowAsync();
+                }
+            }
+        }
     }
 }
